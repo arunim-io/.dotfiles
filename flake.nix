4ca4fba1f9cc8b98b9ef1af9281448d2baf1d1be
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-06-20 01:05:42 +0600

    init
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+        snowfall-lib = {
+            url = "github:snowfallorg/lib";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = inputs:
+        inputs.snowfall-lib.mkFlake {
+            # You must provide our flake inputs to Snowfall Lib.
+            inherit inputs;
+
+            # The `src` must be the root of the flake. See configuration
+            # in the next section for information on how you can move your
+            # Nix files to a separate directory.
+            src = ./.;
+
+	    snowfall.user = {
+		enable = true;
+		name = "arunim";
+	    };
+        };
+}
