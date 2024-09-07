2c76902a28d665e1387378ae8ce961512cfeb1e3
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-09-07 17:48:57 +0600

    nvim: add one dark color scheme package

---
+++ configs/nvim/lua/config/plugins/colourschemes.lua
@@ -0,0 +1,15 @@
+--- @type LazySpec
+return {
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme("onedark")
+    end,
+    opts = {
+      style = "warmer",
+      lualine = { transparent = true },
+    },
+  },
+}
